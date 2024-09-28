3a6ff87d35c107b3d9defee9746474e62ee968ab
System Administrator <root@mengmac.local>
2024-09-29 01:02:16 +0800

    Site updated: 2024-09-29 01:02:15

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/28/hello-world/","2024/09/29/Test/"];function toRandomPost(){
+var posts=["2024/09/29/Test/","2024/09/28/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
